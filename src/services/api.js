7ad1ad6ab576b62b83be23bcf48915aa607f3282
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-03-03 10:54:00 -0300

    new link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://servidor-cadastro-de-usuario-eta.vercel.app/"
 })
 
 export default api
