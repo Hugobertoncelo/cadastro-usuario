324ec88096d5ed88645f11eb2e6bb13532944821
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-03-03 11:07:39 -0300

    deploy adjusts

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://servidor-cadastro-de-usuario-eta.vercel.app/"
+  baseURL: "https://servidor-cadastro-de-usuario.onrender.com"
 })
 
 export default api
